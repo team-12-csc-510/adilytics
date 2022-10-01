ea088e2684aeffab506df6ff8fecb55b3dafb803
aditya <adtewari@ncsu.edu>
2022-10-01 17:37:23 -0400

    Merge branch 'main' of https://github.com/team-12-csc-510/adilytics
    merging remote changes

--- src/models/location_model.py
+++ src/models/location_model.py
@@ -8,13 +8,15 @@ from src.models import PyObjectId
 
 class LocationModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    name: str = Field(...)
+    City: str = Field(...)
+    State: str = Field(...)
 
     class Config:
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
-        schema_extra = {"example": {"name": "Raleigh"}}
+        schema_extra = {"example": {"City": "Raleigh",
+                                    "State": "North Carolina"}}
 
 
 class UpdateLocationModel(BaseModel):
@@ -23,4 +25,5 @@ class UpdateLocationModel(BaseModel):
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
-        schema_extra = {"example": {"name": "Raleigh"}}
+        schema_extra = {"example": {"City": "Raleigh",
+                                    "State": "North Carolina"}}
