b295283bd8b876b6b9c569cb8112304157579186
Saksham <sthakur5@ncsu.edu>
2022-10-04 15:38:28 -0400

    Added main index service.

--- src/services/index_service.py
+++ src/services/index_service.py
@@ -5,7 +5,10 @@ from fastapi.encoders import jsonable_encoder
 from src.database.init_db import Database
 from src.models.ad_model import AdModel, UpdateAdModel
 from src.routes.company_route import list_companies
+from src.services.ad_service import get_conversions
+from src.services.click_service import list_all_clicks, get_total_clicks
 from src.services.location_service import list_locations
+from src.services.user_service import get_total_sessions
 from src.utils.database_const import Collections, Databases
 
 ad_db = Database()
@@ -14,5 +17,7 @@ ad_db.collection = Collections.ad.name
 
 
 async def create_obj():
-    p = await list_locations()
-    return p
+    # click_count = await list_all_clicks()
+    # total_sessions = await get_total_clicks()
+    total_conversion= await get_conversions()
+    return total_conversion
