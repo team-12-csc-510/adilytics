adf5668c64c4d6c3f9cf360cb07d9d41bfda1a84
nbhagat2 <bhagat.naman@gmail.com>
2022-10-05 14:31:07 -0400

    updated locust script for ad creation

--- locust_scripts/ad_creation.py
+++ locust_scripts/ad_creation.py
@@ -1,32 +1,34 @@
 """
 This locust script will create ads that will be displayed to users.
+Number of threads = number of ads to be created
 """
 import json
-from random import randint, randrange
+from random import randint, randrange, uniform
 
-from locust import HttpUser, between, task
+from locust import HttpUser, task
 from locust.exception import StopUser
-from random_username.generate import generate_username
 
 
 class QuickstartUser(HttpUser):
     def on_start(self):
 
-        self.all_locations = self.client.get("/locations/")
-
-        # all locations in a list of dictionary object; each list element contains keys: "_id", "city", "state"
-        self.all_locations = json.loads(self.all_locations.content)
-        print(self.all_locations[0])
-
         self.ad_types = ["SMALL", "MEDIUM", "LARGE", "VIDEO"]
 
     @task
-    def get_user(self):
+    def create_ads(self):
 
         rand_ad_ind = randrange(0, len(self.ad_types))
         ad_type = self.ad_types[rand_ad_ind]
         company_id = randrange(10e5, 10e8)
-        product_id = randrange(10e5, 10e8)
+
+        # product_id will be randomly selected.
+        # Get all the products.
+        all_products = self.client.get("/product/")
+        all_products = json.loads(all_products.content.decode("utf-8"))
+        # print("All products:", all_products)
+        rand_product_id = randrange(0, len(all_products))
+        product_chosen = all_products[rand_product_id]
+        product_chosen_id = product_chosen["_id"]
 
         is_active_decision = randint(0, 1)
         is_active = False
@@ -37,7 +39,7 @@ class QuickstartUser(HttpUser):
             "/ad/",
             json={
                 "company_id": company_id,
-                "product_id": product_id,
+                "product_id": product_chosen_id,
                 "type": ad_type,
                 "is_active": is_active,
             },
