fad5151c8dcffdf0e8b650fd8175c0a63a5f2320
aditya <adtewari@ncsu.edu>
2022-10-05 14:59:14 -0400

    click fix

--- src/utils/time_utils.py
+++ src/utils/time_utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import pytz  # type:ignore
 
@@ -11,14 +11,3 @@ def stringify(datetime_object: datetime = now()) -> str:
     if datetime_object.tzinfo == "US/Eastern":
         return datetime_object.isoformat()
     raise ValueError("Invalid Time Zone")
-
-
-def str2datetime(dateandtime: str) -> datetime:
-    return datetime.strptime(dateandtime, "%Y-%m-%dT%H:%M:%S%z")
-
-
-def timediff30(diff: timedelta) -> bool:
-    if diff < timedelta(30):
-        return True
-    else:
-        return False
